431522bba923457f33b8f93d83d80966544aaccd
Leonardo Valencia <dleo.101213@gmail.com>
2024-03-31 19:57:37 -0500

    section in components.

---
+++ components/about.php
@@ -0,0 +1,32 @@
+      <!-- ======= About Section ======= -->
+      <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2 pt-3" data-aos="fade-left" data-aos-delay="100">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+            <h3>Bienvenido a Solution tech </h3>
+            <p>
+              En Solution Tech, nos dedicamos a proporcionar soluciones tecnológicas innovadoras y de vanguardia para satisfacer las necesidades de nuestros clientes en un mundo en constante evolución. Desde nuestra fundación, nos hemos comprometido a ofrecer productos y servicios de la más alta calidad, diseñados para mejorar la eficiencia operativa, impulsar el crecimiento empresarial y transformar la manera en que las empresas interactúan con la tecnología.
+            </p>
+
+            <p>
+              Nuestro equipo está formado por profesionales altamente calificados y apasionados por la tecnología, que se esfuerzan por ofrecer soluciones personalizadas que se adapten a las necesidades específicas de cada cliente. Ya sea que esté buscando servicios de consultoría en tecnología, desarrollo de software a medida, implementaciones de sistemas de gestión empresarial, estamos aquí para ayudarlo a alcanzar sus objetivos comerciales.
+            </p>
+
+            <p>
+              Gracias por considerar Solution Tech como su socio tecnológico. Esperamos tener la oportunidad de colaborar con usted y ayudarlo a alcanzar nuevos niveles de éxito en su empresa.
+            </p>
+
+            <p>
+              ¡Bienvenido a Solution Tech!
+            </p>
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- End About Section -->
